8a5cf68acb5ca4379f1e371582e304b5f8703462
mingu-beep <bramurve@gmail.com>
2021-05-26 13:36:01 +0900

    Day3 - middleware

--- src/server.js
+++ src/server.js
@@ -3,17 +3,31 @@ import express from "express";
 const PORT = 4500;
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send("I still love you💛💛");
 };
-
-app.get("/", handleHome);
-
-const handleLogin = (req, res) => {
-  return res.send("login here");
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
 };
-app.get("/login", handleLogin);
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
 const handleListening = () =>
   console.log(`Server Listening on port http://localhost:${PORT}🚀`);
-
 app.listen(PORT, handleListening);
