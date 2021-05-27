371e84b47cd84f0aedfaaf3bce9c34db011cb0c1
mingu-beep <bramurve@gmail.com>
2021-05-27 14:04:05 +0900

    Day4 - add external middleware

--- src/server.js
+++ src/server.js
@@ -1,20 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
-const PORT = 4500;
+const PORT = 4000;
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   return res.send("I still love you💛💛");
@@ -23,7 +13,7 @@ const handleProtected = (req, res) => {
   return res.send("Welcome to the private lounge.");
 };
 
-app.use(logger, privateMiddleware);
+app.use(logger);
 app.get("/", handleHome);
 app.get("/protected", handleProtected);
 
