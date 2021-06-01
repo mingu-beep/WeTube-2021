82fa12b4abad85a0ec7a14a501a2d9c2f3762fd8
mingu-beep <bramurve@gmail.com>
2021-06-01 23:20:37 +0900

    #5.7 ~ #5.10

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,4 +1,40 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Video #1",
+      rating: 4,
+      comments: 2,
+      createdAt: "2 minutes age",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      rating: 2,
+      comments: 86,
+      createdAt: "2 minutes age",
+      views: 5469,
+      id: 2,
+    },
+    {
+      title: "Video #3",
+      rating: 3,
+      comments: 23,
+      createdAt: "2 minutes age",
+      views: 129,
+      id: 3,
+    },
+    {
+      title: "Video #4",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes age",
+      views: 239,
+      id: 4,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const search = (req, res) => res.send("Search");
 
 export const watch = (req, res) => res.render("watch");
