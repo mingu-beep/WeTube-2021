2716df46b7a2d1da7ec09f716467464ea94577ce
mingu-beep <bramurve@gmail.com>
2021-06-02 23:59:44 +0900

    Day 10(1/2)

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,43 +1,53 @@
+const videos = [
+  {
+    title: "Video #1",
+    rating: 4,
+    comments: 2,
+    createdAt: "2 minutes age",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    rating: 2,
+    comments: 86,
+    createdAt: "2 minutes age",
+    views: 5469,
+    id: 2,
+  },
+  {
+    title: "Video #3",
+    rating: 3,
+    comments: 23,
+    createdAt: "2 minutes age",
+    views: 129,
+    id: 3,
+  },
+  {
+    title: "Video #4",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes age",
+    views: 239,
+    id: 4,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Video #1",
-      rating: 4,
-      comments: 2,
-      createdAt: "2 minutes age",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      rating: 2,
-      comments: 86,
-      createdAt: "2 minutes age",
-      views: 5469,
-      id: 2,
-    },
-    {
-      title: "Video #3",
-      rating: 3,
-      comments: 23,
-      createdAt: "2 minutes age",
-      views: 129,
-      id: 3,
-    },
-    {
-      title: "Video #4",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes age",
-      views: 239,
-      id: 4,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const search = (req, res) => res.send("Search");
 
-export const watch = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching : ${video.title}`, video });
+};
+export const getedit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing : ${video.title}`, video });
+};
+export const postedit = (req, res) => {};
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -12,6 +12,7 @@ const videoRouter = express.Router();
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.post("/:id(\\d+)/edit");
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
