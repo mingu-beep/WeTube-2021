2539775385a37c342edfdce3d1f8388af691fc77
mingu-beep <bramurve@gmail.com>
2021-06-11 22:53:27 +0900

    Day 18 and 19

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,67 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Log in");
+import User from "../models/User";
+import bcrypt from "bcrypt";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  console.log(req.body);
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match. ",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken",
+    });
+  }
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    res.redirect("/login");
+  } catch {
+    return res.status(404).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  // check if account exists
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  // check if password correct
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+  req.session.loggedIn = true;
+  req.session.user = user;
+  console.log("LOG USER IN COMING SOON!!!");
+  res.redirect("/");
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const deleteUser = (req, res) => res.send("Delete User");
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -11,9 +13,19 @@ const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(localsMiddleware);
+
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
